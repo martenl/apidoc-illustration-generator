e3249c9d29d31bc4986a538838bd73e2fa2eaf76
mlosansky <Marten.Losansky@brands4friends.de>
2016-02-19 16:16:48 +0100

    Added test for SchemaParser

---
+++ schema-domain/src/test/java/de/brands4friends/aig/parser/SchemaParserTest.java
@@ -0,0 +1,28 @@
+package de.brands4friends.aig.parser;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SchemaParserTest {
+
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testReadFromFile() throws Exception {
+
+    }
+
+    @Test
+    public void testYamlParser() throws Exception {
+
+    }
+
+    @Test
+    public void testJsonParser() throws Exception {
+        SchemaParser.jsonParser();
+    }
+}
